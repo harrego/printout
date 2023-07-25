abd31c4131ca19338caa9b3767f2dd236d4a0028
Harry Stanton <harry@harrystanton.com>
2023-07-25 23:35:52 +0100

    feat: completed new ui -- now working app

--- index.js
+++ index.js
@@ -22,17 +22,33 @@ const db = require("better-sqlite3")("db.sqlite3")
 const dbHelper = require("./src/db")
 dbHelper.setup(db)
 
+const urlPrefix = "printout"
+const url = `http://localhost:3000/${urlPrefix}`
+
+app.set("urlPrefix", urlPrefix)
+app.set("url", url)
+
 app.set("db", db)
 
 app.get("/login", (req, res) => {
     res.render("app/login", {})
 })
 
-app.get("/", (req, res) => {
-    res.render("app/index", {})
+app.use((req, res, next) => {
+    const token = req.cookies.token
+    if (!token) {
+        res.redirect("/login")
+        return
+    }
+    const auth = dbHelper.getAuthFromToken(req.app.get("db"), token)
+    if (!auth) {
+        res.clearCookie("token")
+        res.redirect("/login")
+    }
+    next()
 })
 
-app.get("/posts", (req, res) => {
+app.get("/", (req, res) => {
     const db = req.app.get("db")
     var posts = dbHelper.getRecentPosts(db)
 
@@ -47,6 +63,12 @@ app.get("/posts", (req, res) => {
 
 app.get("/new", (req, res) => {
     res.render("app/new", { page: "new" })
+})
+
+app.get("/feed", (req, res) => {
+    const token = req.cookies.token
+    const feedLink = `${url}/feed/${token}/atom.xml`
+    res.render("app/feed", { page: "feed", feed_link: feedLink })
 })
 
 const port = process.env.PORT || 3000
